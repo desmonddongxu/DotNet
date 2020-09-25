4adf3b683aadd9001ae7a76ef5063d86ce39c4c2
Desmond Xu <M33100@mcap.com>
2020-09-25 17:18:05 -0400

    new

---
+++ Crypto/Tool.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Crypto
+{
+    partial class Tool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTextToDecrypt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtDecrypted = new System.Windows.Forms.TextBox();
+            this.txtHASHed = new System.Windows.Forms.TextBox();
+            this.btnHASH = new System.Windows.Forms.Button();
+            this.lblTextToHASH = new System.Windows.Forms.Label();
+            this.txtToHASH = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rdEncrypt = new System.Windows.Forms.RadioButton();
+            this.rdDecrypt = new System.Windows.Forms.RadioButton();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(432, 45);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(112, 47);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(314, 20);
+            this.txtPlainText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text to Encrypt";
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Location = new System.Drawing.Point(112, 77);
+            this.txtEncrypted.Multiline = true;
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(314, 74);
+            this.txtEncrypted.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Encryption Key";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(112, 15);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(314, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "zI0IL/Hamswr/7lbcRXe0kPhhfMirCXqxyKGaycrX+Q=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // txtTextToDecrypt
+            // 
+            this.txtTextToDecrypt.Location = new System.Drawing.Point(112, 172);
+            this.txtTextToDecrypt.Name = "txtTextToDecrypt";
+            this.txtTextToDecrypt.Size = new System.Drawing.Size(314, 20);
+            this.txtTextToDecrypt.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Text to Decrypt";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(434, 172);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 9;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Location = new System.Drawing.Point(112, 201);
+            this.txtDecrypted.Multiline = true;
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(314, 74);
+            this.txtDecrypted.TabIndex = 10;
+            // 
+            // txtHASHed
+            // 
+            this.txtHASHed.Location = new System.Drawing.Point(114, 325);
+            this.txtHASHed.Multiline = true;
+            this.txtHASHed.Name = "txtHASHed";
+            this.txtHASHed.Size = new System.Drawing.Size(314, 74);
+            this.txtHASHed.TabIndex = 14;
+            // 
+            // btnHASH
+            // 
+            this.btnHASH.Location = new System.Drawing.Point(436, 296);
+            this.btnHASH.Name = "btnHASH";
+            this.btnHASH.Size = new System.Drawing.Size(75, 23);
+            this.btnHASH.TabIndex = 13;
+            this.btnHASH.Text = "HASH";
+            this.btnHASH.UseVisualStyleBackColor = true;
+            this.btnHASH.Click += new System.EventHandler(this.btnHASH_Click);
+            // 
+            // lblTextToHASH
+            // 
+            this.lblTextToHASH.AutoSize = true;
+            this.lblTextToHASH.Location = new System.Drawing.Point(4, 299);
+            this.lblTextToHASH.Name = "lblTextToHASH";
+            this.lblTextToHASH.Size = new System.Drawing.Size(109, 13);
+            this.lblTextToHASH.TabIndex = 12;
+            this.lblTextToHASH.Text = "Text to HASH - SALT";
+            // 
+            // txtToHASH
+            // 
+            this.txtToHASH.Location = new System.Drawing.Point(114, 296);
+            this.txtToHASH.Name = "txtToHASH";
+            this.txtToHASH.Size = new System.Drawing.Size(314, 20);
+            this.txtToHASH.TabIndex = 11;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(1003, 18);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(162, 23);
+            this.btnChooseFile.TabIndex = 15;
+            this.btnChooseFile.Text = "Choose the Read File";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(562, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(603, 352);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(559, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "1st column data (column header not required)";
+            // 
+            // rdEncrypt
+            // 
+            this.rdEncrypt.AutoSize = true;
+            this.rdEncrypt.Checked = true;
+            this.rdEncrypt.Location = new System.Drawing.Point(804, 18);
+            this.rdEncrypt.Name = "rdEncrypt";
+            this.rdEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.rdEncrypt.TabIndex = 19;
+            this.rdEncrypt.TabStop = true;
+            this.rdEncrypt.Text = "Encrypt";
+            this.rdEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // rdDecrypt
+            // 
+            this.rdDecrypt.AutoSize = true;
+            this.rdDecrypt.Location = new System.Drawing.Point(871, 18);
+            this.rdDecrypt.Name = "rdDecrypt";
+            this.rdDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.rdDecrypt.TabIndex = 20;
+            this.rdDecrypt.TabStop = true;
+            this.rdDecrypt.Text = "Decrypt";
+            this.rdDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1090, 406);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Tool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 441);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.rdDecrypt);
+            this.Controls.Add(this.rdEncrypt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.txtHASHed);
+            this.Controls.Add(this.btnHASH);
+            this.Controls.Add(this.lblTextToHASH);
+            this.Controls.Add(this.txtToHASH);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTextToDecrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Tool";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTextToDecrypt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtDecrypted;
+        private System.Windows.Forms.Button btnHASH;
+        private System.Windows.Forms.Label lblTextToHASH;
+        private System.Windows.Forms.TextBox txtToHASH;
+        private System.Windows.Forms.TextBox txtHASHed;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rdEncrypt;
+        private System.Windows.Forms.RadioButton rdDecrypt;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Crypto/Tool.cs
@@ -0,0 +1,268 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crypto
+{
+    public partial class Tool : Form
+    {
+        public const string _sharedKey = "zI0IL/Hamswr/7lbcRXe0kPhhfMirCXqxyKGaycrX+Q=";
+        public const int _saltbytes = 24;
+        public const int _iterations = 10000;
+        public const int _codeLength = 6;
+
+        public Tool()
+        {
+            InitializeComponent();
+        }
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption)
+            {
+                Form prompt = new Form()
+                {
+                    Width = 500,
+                    Height = 150,
+                    FormBorderStyle = FormBorderStyle.FixedDialog,
+                    Text = caption,
+                    StartPosition = FormStartPosition.CenterScreen
+                };
+                Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+                TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+                Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+                confirmation.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(textBox);
+                prompt.Controls.Add(confirmation);
+                prompt.Controls.Add(textLabel);
+                prompt.AcceptButton = confirmation;
+
+                return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            txtEncrypted.Text = Encrypt(this.txtPlainText.Text);
+        }
+
+        public string Encrypt(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText))
+            {
+                return string.Empty;
+            }
+
+            // Get the bytes of the string
+            var bytesToBeEncrypted = Encoding.UTF8.GetBytes(plainText);
+            var keyBytes = Encoding.UTF8.GetBytes(_sharedKey);
+
+            // Hash the key with SHA256
+            keyBytes = SHA256.Create().ComputeHash(keyBytes);
+
+            var bytesEncrypted = Encrypt(bytesToBeEncrypted, keyBytes);
+
+            return Convert.ToBase64String(bytesEncrypted);
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            if (string.IsNullOrEmpty(encryptedText))
+            {
+                return string.Empty;
+            }
+
+            // Get the bytes of the string
+            var bytesToBeDecrypted = Convert.FromBase64String(encryptedText);
+            var keyBytes = Encoding.UTF8.GetBytes(_sharedKey);
+
+            keyBytes = SHA256.Create().ComputeHash(keyBytes);
+
+            var bytesDecrypted = Decrypt(bytesToBeDecrypted, keyBytes);
+
+            return Encoding.UTF8.GetString(bytesDecrypted);
+        }
+
+        private static byte[] Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // The salt bytes must be at least 8 bytes.
+            var saltBytes = new byte[_saltbytes];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    using (var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000))
+                    {
+                        aes.KeySize = 256;
+                        aes.BlockSize = 128;
+                        aes.Key = key.GetBytes(aes.KeySize / 8);
+                        aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                        aes.Mode = CipherMode.CBC;
+
+                        using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                            cs.Close();
+                        }
+
+                        encryptedBytes = ms.ToArray();
+                    }
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private static byte[] Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // The salt bytes must be at least 8 bytes.
+            var saltBytes = new byte[_saltbytes];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    using (var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000))
+                    {
+                        aes.KeySize = 256;
+                        aes.BlockSize = 128;
+                        aes.Key = key.GetBytes(aes.KeySize / 8);
+                        aes.IV = key.GetBytes(aes.BlockSize / 8);
+                        aes.Mode = CipherMode.CBC;
+
+                        using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                            cs.Close();
+                        }
+
+                        decryptedBytes = ms.ToArray();
+                    }
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            this.txtDecrypted.Text = Decrypt(this.txtTextToDecrypt.Text);
+        }
+
+        private void btnHASH_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.txtToHASH.Text))
+            {
+                txtToHASH.Text = string.Empty;
+            }
+
+            // generate a 128-bit salt using a secure PRNG
+            var salt = Encoding.UTF8.GetBytes(_sharedKey);
+
+            // derive a 256-bit sub-key (use HMACSHA1 with 10,000 iterations)
+            var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: this.txtToHASH.Text,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: _iterations,
+                numBytesRequested: 32));
+
+            this.txtHASHed.Text = hashed;
+        }
+
+        public DataTable ReadExcel(string fileName, string fileExt)
+        {
+            string conn = string.Empty;
+
+            DataTable dtexcel = new DataTable();
+            dtexcel.Columns.Add("Data", typeof(String));
+            dtexcel.Columns.Add("Result", typeof(String));
+
+
+            if (fileExt.CompareTo(".xls") == 0)
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+            else
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [Sheet1$]", con); //here we read data from sheet1  
+
+                    DataSet users = new DataSet();
+                    oleAdpt.Fill(users, "users");
+
+                    //j = users.Rows.Count;
+                    var stringlist = users.Tables[0].Rows.Cast<DataRow>().Select(dr => dr[0].ToString()).ToList();
+
+                    dtexcel.Clear();
+
+                    for (var i = 0; i < stringlist.Count; i++)
+                    {
+                        DataRow row = dtexcel.NewRow();
+                        row["Data"] = stringlist[i].ToString();
+                        row["Result"] = this.rdEncrypt.Checked ? Encrypt(stringlist[i].ToString()) : Decrypt(stringlist[i].ToString());
+                        dtexcel.Rows.Add(row);
+                    }
+
+                    dtexcel.Columns.Remove("F1");
+
+                    oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+                }
+                catch { }
+            }
+            return dtexcel;
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+            string fileExt = string.Empty;
+            OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file  
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK) //if there is a file choosen by the user  
+            {
+                filePath = file.FileName; //get the path of the file  
+                fileExt = Path.GetExtension(filePath); //get the file extension  
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        DataTable dtExcel = new DataTable();
+                        dtExcel = ReadExcel(filePath, fileExt); //read excel file  
+                        dataGridView1.Visible = true;
+                        dataGridView1.DataSource = dtExcel;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error  
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+        }
+    }
+}
